75776e523e8c8bf16b0c1525f17e2dcd4ffe67ac
ninenine <davisjwn@gmail.com>
2025-12-28 15:58:33 +0300

    test: add msw for api mocking

--- web/src/test/setup.ts
+++ web/src/test/setup.ts
@@ -1,2 +1,13 @@
 import '@testing-library/jest-dom/vitest'
 import 'fake-indexeddb/auto'
+import { beforeAll, afterEach, afterAll } from 'vitest'
+import { server } from './mocks/server'
+
+// Start MSW server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }))
+
+// Reset handlers after each test
+afterEach(() => server.resetHandlers())
+
+// Clean up after all tests
+afterAll(() => server.close())
